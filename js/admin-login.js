db508cd07b5b9d4bad85b4ccb3d06874ddf91e47
1lch2 <1lch2@163.com>
2016-12-24 19:49:25 +0800

    管理员登录与注册的js
    普通用户的js的翻版，不一定管用。。。

--- js/admin-login.js
+++ js/admin-login.js
@@ -1,1 +1,36 @@
-//管理员登录
+$(document).ready(function() {
+    $('#login').click(userLogin);
+    checkAuth();
+});
+
+function userLogin() {
+    $.ajax({
+			type: "POST",
+			url: "API/admin-login.php",
+			dataType:"json",
+			data:{
+                "userName": $('#userName').val(),
+                "pwd": $('#pwd').val()
+            },
+			success: function(data){
+				if(data.result == "success"){
+					location.href='order.html';
+				}
+				else Materialize.toast("用户名或密码错误", 6000);
+			},
+			error: function (XMLHttpRequest, textStatus, errorThrown){    
+			}
+		});
+}
+
+function checkAuth() {
+    $.getJSON("API/me.php", function(data) {
+        if(data.result == "success") {
+            if(data.admin == 0) {
+                location.href='order.html';
+            } else {
+                location.href='admin.html';
+            }
+        }
+    });
+}
